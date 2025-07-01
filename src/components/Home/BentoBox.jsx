a768ba3f80f7a7423d2349b999701db8fc15b091
DharmBharodiya <dharmbharodiya@gmail.com>
2025-07-02 01:38:39 +0530

    completed homepage and added flower and hover animations

---
+++ src/components/Home/BentoBox.jsx
@@ -0,0 +1,46 @@
+import { ArrowUpRight, Repeat } from 'lucide-react'
+import { motion } from 'framer-motion'
+
+function BentoBox({className = "", width, heading, description, flowerClass, flowerClass2}) {
+
+  return (
+    <div className={`${width} ${className} cursor-pointer relative h-35 sm:h-40 rounded-2xl p-4 hover:scale-101 hover:z-10000 transition duration-75`}>
+        <h1
+        className={`text-white text-2xl sm:text-3xl font-bold group font-space-grotesk`}>
+            {heading}
+            <ArrowUpRight
+            className='w-6 h-6 inline-flex relative group-hover:-top-0.5 group-hover:-right-0.5 group-hover:scale-130 transition duration-75 '
+            />
+        </h1>
+        <p className='text-white/80 text-10px sm:text-[15px] break-words font-space-grotesk'>{description}</p>
+        <motion.img
+          src="/images/flower.png"
+          alt="flower"
+          className={`${flowerClass} z-1000000`}
+          animate={{ rotate: 360 }}
+          initial={{ rotate: 0 }}
+          transition={{
+            repeat: Infinity,
+            ease: 'linear',
+            duration: 8, // 10 seconds for a moderate spin speed
+            repeatType: 'loop',
+          }}
+        />
+        <motion.img
+          src="/images/flower.png"
+          alt="flower"
+          className={`${flowerClass2} z-1000000`}
+          animate={{ rotate: 360 }}
+          initial={{ rotate: 0 }}
+          transition={{
+            repeat: Infinity,
+            ease: 'linear',
+            duration: 8, // 10 seconds for a moderate spin speed
+            repeatType: 'loop',
+          }}
+        />
+    </div> 
+  )
+}
+
+export default BentoBox
--- src/components/Home/MainPara.jsx
+++ src/components/Home/MainPara.jsx
@@ -1,17 +1,60 @@
+import { useState } from 'react'
+import BentoBox from './BentoBox'
 import ThemeBtn from './ThemeBtn'
 import { ArrowUpRight } from 'lucide-react'
 
 function MainPara() {
+
   return (
-    <div className='flex justify-center items-center selection:bg-red-500'>
-        <div>
-            <h1 className='text-white font-geist font-extrabold text-[70px]' md-0>Dharm<ThemeBtn/></h1>
-            <p className='text-white text-[18px] mt-0'>Pixels. Purpose. Possibility.</p>
-            <p className='text-[#bbbbbb] text-[18px] max-w-xs'>i  turn thoughts into threads of code and color—crafting digital spaces where design breathes, interaction dances, and ideas come alive.</p>
-            <ArrowUpRight size={46} color='white'/>
+    <div className='flex justify-center flex-col items-center selection:bg-red-500'>
+        <div className='flex flex-col-reverse sm:flex-row justify-center items-center mb-10 w-full'>
+            <div className='flex-1 flex flex-col justify-center items-center sm:items-end'>
+              <h1 className='text-white font-geist font-extrabold text-[60px] sm:text-[70px] mb-0'>Dharm<ThemeBtn/></h1>
+              <p className='text-white text-[18px] mt-0 text-center sm:text-green-500'>Pixels. Purpose. Possibility.</p>
+              <p className='text-[#bbbbbb] text-[16px] max-w-xs leading-5 text-center'>i  turn thoughts into threads of code and color—crafting digital spaces where design breathes, interaction dances, and ideas come alive.</p>
+            </div>
+            <div className='flex-1 flex justify-center sm:justify-start sm:mb-0'>
+              <img src="../public/images/dhrm1.png" alt="dhrmcharacter" className='w-[175px] h-[175px] sm:w-[200px] sm:h-[200px]' />
+            </div>
+            {/* <ArrowUpRight size={46} color='white'/> */}
         </div>
-        <div>
-            <img src="../public/images/dhrm1.png" alt="dhrmcharacter" className='w-[200px] h-[200px]'/>
+        
+        <div className='grid grid-cols-6 w-full p-4 gap-2 sm:w-[700px]'>
+            <BentoBox
+            className='bg-gradient-to-r from-[#020024]/25 via-[#090979]/20 to-[#00D4FF]/30'
+            width="col-span-6 sm:col-span-2"
+            heading="About"
+            description="get to know dharm :) also some cool photographs & artworks"
+            flowerClass="absolute -right-1 -top-8  sm:-right-7 sm:-top-8 w-18 h-18 sm:w-22 sm:h-22"
+            flowerClass2="hidden"
+            />
+
+            <BentoBox
+            className='bg-[linear-gradient(90deg,_rgba(26,0,51,0.2)_0%,_rgba(76,29,149,0.25)_50%,_rgba(255,0,150,0.30)_100%)]'
+            heading="Projects"
+            description="see what dharm is makes hehe"
+            width="col-span-3 sm:col-span-4"
+            flowerClass="absolute hidden sm:block -right-7 top-10 w-15 h-15"
+            flowerClass2="hidden"
+            />
+
+            <BentoBox
+            className='bg-[linear-gradient(90deg,_rgba(51,0,0,0.2)_0%,_rgba(136,0,0,0.25)_50%,_rgba(255,69,0,0.3)_100%)]'
+            heading="Blogs"
+            description="i do write sometimes"
+            width="col-span-3 sm:col-span-4"
+            flowerClass="absolute hidden sm:block -left-8 -top-6 w-13 h-13"
+            flowerClass2="hidden"
+            />
+
+            <BentoBox
+            className='bg-[linear-gradient(90deg,_rgba(0,51,51,0.2)_0%,_rgba(0,128,128,0.25)_50%,_rgba(57,255,20,0.3)_100%)]'
+            heading="Socials"
+            description="where can you find me?"
+            width="col-span-6 sm:col-span-2"
+            flowerClass="absolute left-65 top-28 w-12 h-12 sm:-left-8 sm:top-30 sm:w-13 sm:h-13"
+            flowerClass2="block w-10 h-10 -top-6 -left-4 absolute sm:hidden"
+            />
         </div>
     </div>
   )
