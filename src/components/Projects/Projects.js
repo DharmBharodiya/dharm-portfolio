9e840d3512817dcd5b6f9a71076e8c7746107729
DharmBharodiya <dharmbharodiya@gmail.com>
2025-07-31 23:09:08 +0530

    ADD: Added a new project to the projects section.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,24 +21,25 @@ export const projects = [
         src:"https://github.com/DharmBharodiya/OpenCV"
     },
     {
-        title: "A personal portfolio",
-        description: "Designed and developed a portfolio website to showcase projects and skills",
-        bg: "bg-[linear-gradient(90deg,_rgba(0,51,51,1)_0%,_rgba(0,128,128,1)_50%,_rgba(57,255,20,1)_100%)] dark:bg-[linear-gradient(90deg,_rgba(0,51,51,0.6)_0%,_rgba(0,128,128,0.65)_50%,_rgba(57,255,20,0.70)_100%)] transition-colors duration-200",
-        flowerClass: "absolute w-10 h-10 -bottom-4 left-1/2 -translate-x-1/2",
-        src:"https://dharm-portfolio-gamma.vercel.app/"
+        title: "Note-Taking App",
+        description: "A minimal, distraction free note taking app",
+        bg: "bg-[linear-gradient(90deg,_rgba(0,0,51,1)_0%,_rgba(0,51,102,1)_50%,_rgba(0,102,204,1)_100%)] dark:bg-[linear-gradient(90deg,_rgba(0,0,51,0.6)_0%,_rgba(0,51,102,0.65)_50%,_rgba(0,102,204,0.70)_100%)] transition-colors duration-200",
+        flowerClass: "absolute w-10 h-10 -bottom-5 left-5",
+        src: "https://brad-react.vercel.app/"
     },
-    // {
-    //     title: "completed online course",
-    //     description: "Finished a 10-hour course on web development and earned a certificate",
-    //     bg: "bg-[linear-gradient(90deg,_rgba(0,51,102,1)_0%,_rgba(0,102,204,1)_50%,_rgba(0,153,255,1)_100%)] dark:bg-[linear-gradient(90deg,_rgba(0,51,102,0.6)_0%,_rgba(0,102,204,0.65)_50%,_rgba(0,153,255,0.70)_100%)] transition-colors duration-200",
-    //     flowerClass: "absolute w-10 h-10 -bottom-5 left-5",
-    // },
     {
         title: "Misson Management App",
         description: "Learnt & built this during my time at tatvasoft.",
         bg: "bg-[linear-gradient(90deg,_rgba(51,0,51,1)_0%,_rgba(128,0,128,1)_50%,_rgba(255,0,255,1)_100%)] dark:bg-[linear-gradient(90deg,_rgba(51,0,51,0.6)_0%,_rgba(128,0,128,0.65)_50%,_rgba(255,0,255,0.70)_100%)] transition-colors duration-200",
         flowerClass: "absolute w-12 h-12 -bottom-6 right-10",
         src:"https://github.com/DharmBharodiya/Mission-App"
+    },
+    {
+        title: "A personal portfolio",
+        description: "Designed and developed a portfolio website to showcase projects and skills",
+         bg: "bg-[linear-gradient(90deg,_rgba(0,51,0,1)_0%,_rgba(0,128,0,1)_50%,_rgba(0,255,0,1)_100%)] dark:bg-[linear-gradient(90deg,_rgba(0,51,0,0.6)_0%,_rgba(0,128,0,0.65)_50%,_rgba(0,255,0,0.70)_100%)] transition-colors duration-200",
+        flowerClass: "absolute w-10 h-10 -bottom-4 left-1/2 -translate-x-1/2",
+        src:"https://dharm-portfolio-gamma.vercel.app/"
     },
     // {
     //     title: "completed coding challenge",
